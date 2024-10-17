6cc23995dea61eb8f24f2093c59664f656b3673a
christian <christiangomez844@gmail.com>
2024-10-17 11:49:58 -0600

    donut_tile

--- lib/tab/Donut_tab.dart
+++ lib/tab/Donut_tab.dart
@@ -1,35 +1,37 @@
 import 'package:flutter/material.dart';
 
-import '../icons/utils/donut_tile.dart';
+import '../utils/donut_tile.dart';
 
 class DonutTab extends StatelessWidget {
-    //list of donuts
-    final List donutsOnSale = [
-      //[donutFlavor, donutPrice, donutColor, imageName]
-      ["Ice Cream","36", Colors.blue,"lib/images/icecream_donut.png"],
-      ["Strawberry", "45",Colors.red, "lib/images/strawberry_donut.png"],
-      ["Grape Ape", "84", Colors.purple,"lib/images/grape_donut.png"],
-      ["Choco","95", Colors.brown, "lib/images/chocolate_donut.png"],
+  final List donutsOnsale = [
+    ["Ice Cream","36", Colors.blue,"lib/images/chocolate_donut.png"],
+    ["Strawberry","45", Colors.brown,"lib/images/grape_donut.png"],
+    ["Grape Ape","84", Colors.red,"lib/images/icecream_donut.png"],
+    ["Choco","95", Colors.pink,"lib/images/strawberry_donut.png"],
 
+  ];
 
-    ];
-
-   DonutTab({super.key});
+  DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      itemCount: donutsOnSale.length,//longitud de los elementos
+      itemCount: donutsOnsale.length,
       padding: const EdgeInsets.all(12),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),//dos columnas
-      itemBuilder:(context, index){
+      gridDelegate:
+      const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2, childAspectRatio: 1/1.5,
+        // crossAxisSpacing: 5,
+        // mainAxisSpacing: 5,
+      ),
+      itemBuilder: (context, index){
         return DonutTile(
-          donutFlavor: donutsOnSale[index][0],
-          donutPrice: donutsOnSale[index][1],
-          donutColor: donutsOnSale[index][2],
-          imageName: donutsOnSale[index][3],
+          donutFlavor: donutsOnsale[index][0],
+          donutPrice: donutsOnsale[index][1],
+          donutColor: donutsOnsale[index][2],
+          imageName: donutsOnsale[index][3],
         );
-      }
-    );//GridView.builder
+      },
+    );
   }
 }
