adb223e4735997634f0e6a456d2e9cc4d963b55c
christian <christiangomez844@gmail.com>
2024-10-17 11:17:38 -0600

    donut_tile

--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -1,1 +1,55 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class DonutTile extends StatelessWidget {
+  final String donutFlavor;
+  final String donutPrice;
+  final dynamic donutColor;
+  final String imageName;
+  final double borderRadius = 24;
+
+  const DonutTile(
+    {super.key,
+      required this.donutFlavor,
+      required this.donutPrice,
+      this.donutColor,
+      required this.imageName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: const EdgeInsets.all(12),
+    child: Container(
+      decoration: BoxDecoration(color: donutColor[50], borderRadius: BorderRadius.circular(borderRadius)),
+      child: Column(
+        //Donut price
+        children: [Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+          Container(
+            decoration: BoxDecoration(color: donutColor[100],
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(borderRadius),
+              bottomLeft: Radius.circular(borderRadius),
+            )),
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
+            child: Text('\$$donutPrice',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: donutColor[800]),
+            ),
+          )
+        ],
+        ),
+        //Donut picture
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+          child: Image.asset(imageName),),
+        //Donut flavor text
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 4),
+          child: Text(donutFlavor, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14, color: donutColor[1000])),
+        ),
+        const Text('Dunkin\'s'),
+        //Love icon + add button
+        ],
+      ),
+    ),);
+  }
+}
