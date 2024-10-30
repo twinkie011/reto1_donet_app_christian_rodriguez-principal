1887da26f5cb30d02d0b455eda8ec894e682fd52
Gaspar Euan <91715893+EmiiEu4n@users.noreply.github.com>
2024-10-29 20:21:09 -0600

    Conexión-a-firestore-database
    Archivos modificados para la conexion a la base de datos

--- lib/tab/Pizza_tab.dart
+++ lib/tab/Pizza_tab.dart
@@ -1,40 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../utils/pizza_tile.dart';
 
-class PizzaTab extends StatelessWidget {
-  final List pizzasOnSale = [
-    ["Margherita", "80", Colors.red, "lib/images/margherita_pizza.png"],
-    ["Pepperoni", "90", Colors.orange, "lib/images/pepperoni_pizza.png"],
-    ["BBQ Chicken", "100", Colors.brown, "lib/images/bbq_pizza.png"],
-    ["Veggie", "70", Colors.green, "lib/images/veggie_pizza.png"],
-    ["Hawaiian", "85", Colors.yellow, "lib/images/hawaiian_pizza.png"],
-    ["Buffalo Chicken", "95", Colors.deepOrange, "lib/images/buffalo_pizza.png"],
-    ["Meat Lovers", "110", Colors.redAccent, "lib/images/meat_lovers_pizza.png"],
-    ["Cheese Lovers", "75", Colors.amber, "lib/images/cheese_lovers_pizza.png"],
-  ];
-
+class PizzaTab extends StatefulWidget {
   final Function(double) addItem; // Función para agregar un item
 
-  PizzaTab({super.key, required this.addItem}); // Recibir la función en el constructor
+  const PizzaTab({super.key, required this.addItem}); // Recibe la función en el constructor
+
+  @override
+  _PizzaTabState createState() => _PizzaTabState();
+}
+
+class _PizzaTabState extends State<PizzaTab> {
+  final CollectionReference pizzasRef = FirebaseFirestore.instance
+      .collection('pizzas'); // Referencia a la colección en Firestore
+  List<dynamic> pizzasOnSale = []; // Lista vacía que se llenará con los datos de Firestore
+
+  @override
+  void initState() {
+    super.initState();
+    getPizzasData(); // Llamamos la función para obtener los datos al iniciar
+  }
+
+  void getPizzasData() async {
+    try {
+      // Obtener los datos de la colección 'pizzas'
+      pizzasRef.snapshots().listen((snapshot) {
+        setState(() {
+          // Convertir los documentos en una lista
+          pizzasOnSale = snapshot.docs.map((doc) => doc.data()).toList();
+        });
+      });
+    } catch (e) {
+      // Manejar errores
+      print('Error al obtener datos: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      itemCount: pizzasOnSale.length,
+      itemCount: pizzasOnSale.length, // longitud del arreglo dinámico
       padding: const EdgeInsets.all(12),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 1 / 1.5,
       ),
       itemBuilder: (context, index) {
+        var pizza = pizzasOnSale[index];
+
+        // Imprimir valores para debugging
+        print(
+            'Nombre: ${pizza['nombre']}, Precio: ${pizza['precio']}, Color: ${pizza['color']}');
+
         return PizzaTile(
-          pizzaFlavor: pizzasOnSale[index][0],
-          pizzaPrice: pizzasOnSale[index][1],
-          pizzaColor: pizzasOnSale[index][2],
-          imageName: pizzasOnSale[index][3],
+          pizzaFlavor: pizza['nombre'] is String
+              ? pizza['nombre']
+              : pizza['nombre'].toString(), // Asegúrate de que sea un String
+          pizzaPrice: pizza['precio'] is String
+              ? pizza['precio']
+              : pizza['precio'].toString(), // Asegúrate de que sea un String
+          pizzaColor: Color(pizza['color']), // Convertir el entero en un Color
+          imageName: pizza['img'], // Cambié 'image' a 'img'
           addToCart: () {
-            addItem(double.parse(pizzasOnSale[index][1])); // Llama a la función addItem con el precio
-          }, pizzaName: '',
+            widget.addItem(double.parse(pizza['precio']
+                .toString())); // Llama a la función addItem con el precio
+          },
         );
       },
     );
